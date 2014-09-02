066b7e4d69b859e261c35871fdf8286d3e45c1d4
hiroomi onoe <onoe@flight-inc.net>
2014-09-02 14:48:30 +0900

    3行出してみる。
    3回目？

--- src/main/Test.java
+++ src/main/Test.java
@@ -17,6 +17,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Flight!");
 		System.out.println("Hello Flight!");
+		System.out.println("Hello Flight!");
 	}
 	
 }
