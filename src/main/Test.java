637743f4106c2f1cb8330ffb04422c1ce3ca883e
hiroomi onoe <onoe@flight-inc.net>
2014-09-02 14:45:25 +0900

    初コミット
    こみっと？

---
+++ src/main/Test.java
@@ -0,0 +1,21 @@
+/* $Id: $ */
+package main;
+
+/**
+ *
+ * Testクラス
+ *
+ * @author onoe
+ *
+ */
+public class Test {
+	
+	
+	public Test() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Flight!");
+	}
+	
+}
